29289f9cec10a7472b54584b3d6138ddb34ae144
Guilherme0112 <guimendesmen124@gmail.com>
2025-01-09 00:39:01 -0300

    Arquivos estáticos NextJS

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.mjs
+export default {
+    output: 'export', // Habilita o comando "next export"
+    images: {
+      unoptimized: true, // Desabilita otimização de imagens para exportação
+    },
+    basePath: '/Guilherme0112.github.io', // Adicione o nome do repositório aqui
+    assetPrefix: '/Guilherme0112.github.io/', // Prefixo para os ativos estáticos
+  };
+  
